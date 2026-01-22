a05b2c8757ae1660f160209366f02710a0cae7a1
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2026-01-22 21:58:06 +0700

    Initial commit

---
+++ TouchGFX/gui/src/welcome_screen/WelcomeView.cpp
@@ -0,0 +1,15 @@
+#include <gui/welcome_screen/WelcomeView.hpp>
+
+WelcomeView::WelcomeView()
+{
+}
+
+void WelcomeView::setupScreen()
+{
+    WelcomeViewBase::setupScreen();
+}
+
+void WelcomeView::tearDownScreen()
+{
+    WelcomeViewBase::tearDownScreen();
+}
